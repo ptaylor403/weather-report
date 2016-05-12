60889ec71436e6f75d29461bf3872fbd8999f724
Trisha <ptaylor403@gmail.com>
2016-05-11 22:51:20 -0400

    saved json response in python file

--- weather.py
+++ weather.py
@@ -11,9 +11,10 @@ data = 'geolookup/conditions/forecast10day/alerts/currenthurricane/astronomy/pws
 def main():
     zipcode = Zipcode.get_zip()
     url = Url.create_url(zipcode)
-    # r = requests.get(lookup_url)
-    # r_json = r.json()
-    # for key, value in r_json.items():
-    #     print(key)
 
 main()
+
+# r = requests.get(lookup_url)
+# r_json = r.json()
+# for key, value in r_json.items():
+#     print(key)
